73b2ea5db0a4f86a17036f73379dc7a7b8ec1192
ericleonen <ericleonen@gmail.com>
2022-05-02 14:02:26 -0700

    Create white screen

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
+import sys
 import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+playing = True
+
+while playing:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            playing = False
+
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
